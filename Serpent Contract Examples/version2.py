3314f7ecb17bacd6877dd68601eb8b44a2b0af90
firesnowball <del@terpmail.umd.edu>
2015-03-25 13:54:31 -0400

    Minor changes to version2

--- Serpent Contract Examples/version2.py
+++ Serpent Contract Examples/version2.py
@@ -38,13 +38,13 @@ def add_player():
 			self.storage["WINNINGS"] = self.storage["WINNINGS"] + msg.value
 			self.storage["player1"] = msg.sender
 			return(1)
-		return (0)
+		return(0)
 	elif not self.storage["player2"]:
 		if msg.value == 1000:
 			self.storage["WINNINGS"] = self.storage["WINNINGS"] + msg.value
 			self.storage["player2"] = msg.sender
 			return(2)
-		return (0)
+		return(0)
 	else:
 		return(0)
 
@@ -54,56 +54,55 @@ def input(player_commitment):
 		return (1)
 	elif self.storage["player2"] ==  msg.sender:
 		self.storage["p2commit"] = player_commitment
-		return (2)
+		return(2)
 	else:
-		return (0)
-
-def check():
-	#check to see if both players have revealed answer
-	if self.storage["p1reveal"] and self.storage["p2reveal"]:
-		#If player 1 wins
-		if self.winnings_table[self.storage["p1value"]][self.storage["p2value"]] == 1:
-			send(self.storage["player1"], self.storage["WINNINGS"])
-			return (1)
-		#If player 2 wins
-		elif self.winnings_table[self.storage["p1value"]][self.storage["p2value"]] == 2:
-			send(self.storage["player2"], self.storage["WINNINGS"])
-			return (2)
-		#If no one wins
-		else:
-			send(self.storage["player1"], 1000)
-			send(self.storage["player2"], 1000)
-			return (0)
-	#if p1 revealed but p2 did not, send money to p1
-	elif if self.storage["p1reveal"] and not self.storage["p2reveal"]:
-		send(self.storage["player1"], self.storage["WINNINGS"])
-		return (1)
-	#if p2 revealed but p1 did not, send money to p2
-	elif if not self.storage["p1reveal"] and self.storage["p2reveal"]:
-		send(self.storage["player2"], self.storage["WINNINGS"])
-		return (2)
-	#if neither p1 nor p2 revealed, keep both of their bets
-	else:
-		return(-1)
+		return(0)
 
 def open(choice, nonce):
 	if self.storage["player1"] == msg.sender:
 		if sha256([choice, nonce], 2) == self.storage["p1commit"]:
 			self.storage["p1value"] = choice
 			self.storage["p1reveal"] = true
-			return (1)
+			return(1)
 		else:
-			return (0)
+			return(0)
 	elif self.storage["player2"] == msg.sender:
 		if sha256([choice, nonce], 2) == self.storage["p2commit"]:
 			self.storage["p2value"] = choice
 			self.storage["p2reveal"] = true
-			return (2)
+			return(2)
 		else:
-			return (0)
+			return(0)
 	else:
-		return (-1)
+		return(-1)
 
+def check():
+	#check to see if both players have revealed answer
+	if self.storage["p1reveal"] and self.storage["p2reveal"]:
+		#If player 1 wins
+		if self.winnings_table[self.storage["p1value"]][self.storage["p2value"]] == 1:
+			send(100,self.storage["player1"], self.storage["WINNINGS"])
+			return(1)
+		#If player 2 wins
+		elif self.winnings_table[self.storage["p1value"]][self.storage["p2value"]] == 2:
+			send(100,self.storage["player2"], self.storage["WINNINGS"])
+			return(2)
+		#If no one wins
+		else:
+			send(100,self.storage["player1"], 1000)
+			send(100,self.storage["player2"], 1000)
+			return(0)
+	#if p1 revealed but p2 did not, send money to p1
+	elif if self.storage["p1reveal"] and not self.storage["p2reveal"]:
+		send(100,self.storage["player1"], self.storage["WINNINGS"])
+		return(1)
+	#if p2 revealed but p1 did not, send money to p2
+	elif if not self.storage["p1reveal"] and self.storage["p2reveal"]:
+		send(100,self.storage["player2"], self.storage["WINNINGS"])
+		return(2)
+	#if neither p1 nor p2 revealed, keep both of their bets
+	else:
+		return(-1)
 
 def balance_check():
 	log(self.storage["player1"].balance)
@@ -116,13 +115,13 @@ translator = abi.ContractTranslator(serpent.mk_full_signature(serpent_code))
 data = translator.encode('add_player', [])
 s = tester.state()
 c = s.evm(evm_code)
-o = translator.decode('add_player', s.send(tester.k0, c, 50, data))
+o = translator.decode('add_player', s.send(tester.k0, c, 1000, data))
 print(o)
 
 data = translator.encode('add_player', [])
 #s = tester.state()
 #c = s.evm(evm_code)
-o = translator.decode('add_player', s.send(tester.k1, c, 50, data))
+o = translator.decode('add_player', s.send(tester.k1, c, 1000, data))
 print(o)
 
 
