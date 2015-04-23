bd975cdfd83d78c10ccdaa1d3f99c54e57cac1b7
firesnowball <del@terpmail.umd.edu>
2015-04-23 01:10:37 -0400

    Added section about calculating gas

--- Serpent Contract Examples/new_bank_contract.py
+++ Serpent Contract Examples/new_bank_contract.py
@@ -3,13 +3,11 @@ from pyethereum import tester, utils, abi
 
 serpent_code = '''
 def deposit():
-	log(tx.gasprice)
 	if not self.storage[msg.sender]:
 		self.storage[msg.sender] = 0
 	self.storage[msg.sender] += msg.value
 	return(1)
 def withdraw(amount):
-	log(tx.gasprice)
 	if self.storage[msg.sender] < amount:
 		return(-1)
 	else:
@@ -17,8 +15,6 @@ def withdraw(amount):
 		send(0, msg.sender, amount)
 		return(1)
 def transfer(amount, destination):
-	log(tx.gasprice)
-	log(tx.gas)
 	if self.storage[msg.sender] < amount:
 		return(-1)
 	else:
@@ -40,36 +36,50 @@ public_k1 = utils.privtoaddr(tester.k1)
 evm_code = serpent.compile(serpent_code)
 translator = abi.ContractTranslator(serpent.mk_full_signature(serpent_code))
 
-data = translator.encode('deposit', [])
 s = tester.state()
+print(s.block.gas_used)
 c = s.evm(evm_code)
-o = translator.decode('deposit', s.send(tester.k0, c, 1000, data))
-print(o)
 
-data = translator.encode('withdraw', [1000])
-o = translator.decode('withdraw', s.send(tester.k0, c, 0, data))
-print(o)
-
-data = translator.encode('withdraw', [1000])
-o = translator.decode('withdraw', s.send(tester.k0, c, 0, data))
-print(o)
+print(s.block.gas_used)
 
 data = translator.encode('deposit', [])
 o = translator.decode('deposit', s.send(tester.k0, c, 1000, data))
 print(o)
+
+print(s.block.gas_used)
+
+data = translator.encode('withdraw', [1000])
+o = translator.decode('withdraw', s.send(tester.k0, c, 0, data))
+print(o)
+
+print(s.block.gas_used)
+
+data = translator.encode('withdraw', [1000])
+o = translator.decode('withdraw', s.send(tester.k0, c, 0, data))
+print(o)
+
+print(s.block.gas_used)
+
+data = translator.encode('deposit', [])
+o = translator.decode('deposit', s.send(tester.k0, c, 1000, data))
+print(o)
+
+print(s.block.gas_used)
 
 data = translator.encode('transfer', [500, public_k1])
 o = translator.decode('transfer', s.send(tester.k0, c, 0, data))
 print(o)
 
+print(s.block.gas_used)
+
 data = translator.encode('balance', [])
 o = translator.decode('balance', s.send(tester.k0, c, 0, data))
 print(o)
+
+print(s.block.gas_used)
 
 data = translator.encode('balance', [])
 o = translator.decode('balance', s.send(tester.k1, c, 0, data))
 print(o)
 
-
-
-
+print(s.block.gas_used)
